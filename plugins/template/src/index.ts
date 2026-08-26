de8ac1b4e35e5a3026e2e145469e02e744eabe6c
Rᴏᴏᴛᴋɪᴛ™ <lucaebraeuer@gmail.com>
2026-08-26 07:39:16 +0200

    Refactor auto quote plugin to use logging
    Removed message patching logic and added logging for load/unload events.

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,37 +1,11 @@
-import { metro } from "@vendetta/metro/common";
-import { before } from "@vendetta/patcher";
-
-let unpatch: (() => void) | null = null;
+import { logger } from "@vendetta";
 
 export default {
     onLoad() {
-        const MessageActions = metro.findByProps("sendMessage");
-
-        if (!MessageActions) return;
-
-        unpatch = before(
-            "sendMessage",
-            MessageActions,
-            (args: any[]) => {
-                for (const arg of args) {
-                    if (
-                        arg &&
-                        typeof arg === "object" &&
-                        typeof arg.content === "string" &&
-                        arg.content.length > 0
-                    ) {
-                        if (!arg.content.startsWith(">>> ")) {
-                            arg.content = `>>> ${arg.content}`;
-                        }
-                        break;
-                    }
-                }
-            }
-        );
+        logger.log("Auto Quote loaded");
     },
 
     onUnload() {
-        unpatch?.();
-        unpatch = null;
+        logger.log("Auto Quote unloaded");
     },
 };
