52dca21971dd78f40bcefc7871ce2d8a47a93c1e
Rᴏᴏᴛᴋɪᴛ™ <lucaebraeuer@gmail.com>
2026-08-26 07:36:09 +0200

    Refactor onLoad and onUnload methods

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,37 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { metro } from "@vendetta/metro/common";
+import { before } from "@vendetta/patcher";
+
+let unpatch: (() => void) | null = null;
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
+    onLoad() {
+        const MessageActions = metro.findByProps("sendMessage");
+
+        if (!MessageActions) return;
+
+        unpatch = before(
+            "sendMessage",
+            MessageActions,
+            (args: any[]) => {
+                for (const arg of args) {
+                    if (
+                        arg &&
+                        typeof arg === "object" &&
+                        typeof arg.content === "string" &&
+                        arg.content.length > 0
+                    ) {
+                        if (!arg.content.startsWith(">>> ")) {
+                            arg.content = `>>> ${arg.content}`;
+                        }
+                        break;
+                    }
+                }
+            }
+        );
     },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
+
+    onUnload() {
+        unpatch?.();
+        unpatch = null;
     },
-    settings: Settings,
-}
+};
